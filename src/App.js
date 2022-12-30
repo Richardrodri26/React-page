a7546485133d575b1242f4a931b4edbd18723313
Richardrodri26 <richardmanuel811@gmail.com>
2022-12-29 21:02:59 -0500

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className='text-4x1 underline'>Hello</h1>
     </div>
   );
 }
