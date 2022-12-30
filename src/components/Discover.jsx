c77cb1509f54ade83da60699d716b1e11db233f0
Luis Angulo <luisangulo97@hotmail.com>
2022-12-29 21:56:53 -0500

    Discover.jsx

---
+++ src/components/Discover.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Discover = () => {
+    return (
+        <>
+        <div>
+        <div>
+        <img src="images/safari_desert.jpg" />
+        </div>
+        <div>
+        </div>
+        </div>
+        <div>
+        </div>
+        <div>
+        </div>
+        </>
+    );
+};
+
+export default Discover
